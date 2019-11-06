071d5d33630dd69c7b92aeb4115f39ac2c6a8168
cassandra.bailey293 <cassandra.bailey293@gmail.com>
2019-11-05 20:39:37 -0500

    add more data

--- app.js
+++ app.js
@@ -47,8 +47,61 @@ function call_results(req, res) {
     ts8: JSON.parse(req.query.total8)
   }
 
+/*
+  Calculated = score4 + score6
+
+Experimental = ⅔*(score1 + score2 + score3 )
+
+Resourceful = ⅔*(score5 + score6 + score7 )
+*/
+
+console.log("TS FOUR:::::", data.ts4);
+console.log("TS FOUR INT?:::::", parseInt(data.ts4));
+
+var calcuated = parseInt(data.ts4) + parseInt(data.ts6);
+var experimental = 0.66*(parseInt(data.ts1) + parseInt(data.ts2) + parseInt(data.ts3));
+var resourceful = 0.66*(parseInt(data.ts5) + parseInt(data.ts6) + parseInt(data.ts7));
+
+let max = Math.max(calcuated, experimental, resourceful);
+
+var persona = "";
+var personaDesc = "";
+
+if (max == calcuated) {
+  persona = "Calculated";
+  personaDesc = "You are calculated in your approach to programming. You are meticulous and thorough, dedicated to understanding the task or problem at a high level.";
+} else if (max == experimental) {
+  persona = "Experimental";
+  personaDesc = "You are experimental in your approach to programming. You prefer tackling the problem or task head-on and getting your hands dirty. You prefer to learn by doing.";
+} else if (max == resourceful) {
+  persona = "Resourceful";
+  personaDesc = "You are resourceful in your approch to programming. You like using (and adding to) your bag of tricks, and you're not afraid to ask for help.";
+}
+
+
+  var debugScoreTotals = [];
+
+  var implScoreTotals = [];
+
+  var verifScoreTotals = [];
+
+  for (i=1;i<=8;i++) {
+    console.log(">>>>DATA.DM: ", data.dm)
+    console.log(">>>>DATA.DM1 backets: ", data.dm[iStr])
+    var iStr = i.toString();
+
+    debugScoreTotals[i] = data.dm[iStr] + data.dc[iStr] + data.df[iStr];
+    implScoreTotals[i] = data.im[iStr] + data.ic[iStr] + data.if[iStr];
+    verifScoreTotals[i] = data.vm[iStr] + data.vc[iStr] + data.vf[iStr];
+    console.log("debugScoreTotals(", i, "): ", debugScoreTotals[i])
+  }
+
+
+
   db.collection("responses").add({
-    data: data
+    data: data,
+    persona: persona,
+    desc: personaDesc
   })
 .then(function(docRef) {
   var str = "";
@@ -58,7 +111,7 @@ function call_results(req, res) {
         str = str.concat(`${doc.id} => ${JSON.stringify(doc.data().data)} \n`);
     });
     console.log("Get success: ", str);
-    res.render('index', {data: data, str: str});
+    res.render('index', {data: data, str: str, persona: persona, desc: personaDesc});
 });
     // res.render('index', {data: data, str: str});
     console.log("Document written with ID: ", docRef.id);
